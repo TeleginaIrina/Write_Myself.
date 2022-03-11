948a72329873309b5ebb058bd0834fcf8cc590d7
Телегина Ирина <ikoska056@gmail.com>
2022-03-11 18:14:04 +0300

    add 002

---
+++ 002/Program.cs
@@ -0,0 +1,18 @@
+﻿// 002. Даны два числа. Показать большее и меньшее.
+
+Console.Clear();
+
+int num1 = new Random().Next(1,100);
+Console.WriteLine("num1 = " + num1);
+
+int num2 = new Random().Next(1,100);
+Console.WriteLine("num2 = " + num2);
+
+if (num1 < num2)
+{
+    Console.WriteLine($"(min = {num1}, max = {num2})");
+}
+else
+{
+    Console.WriteLine($"(min = {num2}, max = {num1})");
+}
