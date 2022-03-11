2137aba84e954703d29ab680d35fe204c460acb1
Телегина Ирина <ikoska056@gmail.com>
2022-03-11 17:54:46 +0300

    add 001

---
+++ 001/Program.cs
@@ -0,0 +1,18 @@
+﻿// 001. По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Clear();
+
+int num1 = new Random().Next(1,10);
+Console.WriteLine("num1 = " + num1);
+
+int num2 = new Random().Next(1,10);
+Console.WriteLine("num2 = " + num2);
+
+if (num1 == num2 * num2)
+{
+    Console.WriteLine($"{num1} является квадратом {num2}");
+}
+else
+{
+    Console.WriteLine($"{num1} НЕ является квадратом {num2}");
+}
